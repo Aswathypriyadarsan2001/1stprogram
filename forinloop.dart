b3d039b6d7a9f4fe37300d7a5c796e5f3b69f965
aswathypriyadarsan <93774022+aswathypriyadarsan@users.noreply.github.com>
2022-08-19 11:20:31 +0530

    hai

--- forinloop.dart
+++ forinloop.dart
@@ -1,2 +1,21 @@
-var name1="Morning1";
-List n=
+void main(){
+  //for each loop
+  List numbers  = <int>[1,2,3,4,5];
+  print("---For Each Loop---");
+  numbers.forEach((element) {
+    print(element);
+  });
+
+  print("---For Loop---");
+  //normal for loop
+  for( int i = 0; i < 5 ; i++){
+    print(numbers[i]);
+  }
+
+  print("---For In Loop---");
+  //for in loop
+  for(int a in numbers){
+    print(a);
+  }
+
+}
